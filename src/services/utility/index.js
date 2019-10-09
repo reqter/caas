c6ba2f5f382ce2f70e1445180791995595afd6ad
saeedPadyab <padyabsaeed@gmail.com>
2019-10-09 20:00:25 +0300

    checking url value

--- src/services/utility/index.js
+++ src/services/utility/index.js
@@ -1,273 +1,273 @@
-import React from 'react'
-import languageManager from './../languageManager'
-const currentLang = languageManager.getCurrentLanguage().name
-const imgs = ['jpg', 'jpeg', 'gif', 'bmp', 'png']
-const videos = ['mp4', '3gp', 'ogg', 'wmv', 'flv', 'avi']
-const audios = ['wav', 'mp3', 'ogg']
+import React from "react";
+import languageManager from "./../languageManager";
+const currentLang = languageManager.getCurrentLanguage().name;
+const imgs = ["jpg", "jpeg", "gif", "bmp", "png"];
+const videos = ["mp4", "3gp", "ogg", "wmv", "flv", "avi"];
+const audios = ["wav", "mp3", "ogg"];
 
 export default {
-  applyeLangs (value) {
-    const langs = languageManager.getAllLanguages()
-    const data = {}
+  applyeLangs(value) {
+    const langs = languageManager.getAllLanguages();
+    const data = {};
     for (const key in langs) {
-      data[key] = value
+      data[key] = value;
     }
-    return data
+    return data;
   },
-  getAssetIconByURL (url, customClass) {
-    if (url) {
+  getAssetIconByURL(url, customClass) {
+    if (url && typeof url === "string") {
       const ext = url
-        .split('/')
+        .split("/")
         .pop()
-        .split('.')
-        .pop()
-      const cls = 'unkownFileType ' + customClass
+        .split(".")
+        .pop();
+      const cls = "unknownFileType " + customClass;
 
       if (!ext) {
         return (
           <div className={cls}>
-            <i className='icon-file-text un-icon' />
-            <span className='un-text'>uknown</span>
+            <i className="icon-file-text un-icon" />
+            <span className="un-text">uknown</span>
           </div>
-        )
+        );
       } else {
         if (imgs.indexOf(ext.toLowerCase()) !== -1) {
-          return <img src={url} alt='' />
+          return <img src={url} alt="" />;
         } else if (videos.indexOf(ext.toLowerCase()) !== -1) {
-          return <i className='icon-video' />
+          return <i className="icon-video" />;
         } else if (audios.indexOf(ext.toLowerCase()) !== -1) {
-          return <i className='icon-audio' />
+          return <i className="icon-audio" />;
         } else {
           return (
             <div className={cls}>
-              <i className='icon-file-text un-icon' />
-              <span className='un-text'>.{ext}</span>
+              <i className="icon-file-text un-icon" />
+              <span className="un-text">.{ext}</span>
             </div>
-          )
+          );
         }
       }
     }
   },
-  getRequestMediaComponentByURL (url, customClass) {
+  getRequestMediaComponentByURL(url, customClass) {
     if (url) {
       const ext = url
-        .split('/')
+        .split("/")
         .pop()
-        .split('.')
-        .pop()
-      const cls = 'unkownFileType ' + customClass
+        .split(".")
+        .pop();
+      const cls = "unkownFileType " + customClass;
 
       if (!ext) {
         return (
           <div className={cls}>
-            <i className='icon-file-text un-icon' />
-            <span className='un-text'>uknown</span>
+            <i className="icon-file-text un-icon" />
+            <span className="un-text">uknown</span>
           </div>
-        )
+        );
       } else {
         if (imgs.indexOf(ext.toLowerCase()) !== -1) {
-          return <img src={url} alt='' />
+          return <img src={url} alt="" />;
         } else if (videos.indexOf(ext.toLowerCase()) !== -1) {
           return (
             <video controls>
               <source src={url} />
             </video>
-          )
+          );
         } else if (audios.indexOf(ext.toLowerCase()) !== -1) {
           return (
             <audio controls>
               <source src={url} />
             </audio>
-          )
+          );
         } else {
           return (
             <div className={cls}>
-              <i className='icon-file-text un-icon' />
-              <span className='un-text'>.{ext}</span>
+              <i className="icon-file-text un-icon" />
+              <span className="un-text">.{ext}</span>
             </div>
-          )
+          );
         }
       }
     }
   },
-  getRequestMediaThumbComponentByURL (url, customClass) {
+  getRequestMediaThumbComponentByURL(url, customClass) {
     if (url) {
       const ext = url
-        .split('/')
+        .split("/")
         .pop()
-        .split('.')
-        .pop()
-      const cls = 'unkownFileType ' + customClass
+        .split(".")
+        .pop();
+      const cls = "unkownFileType " + customClass;
 
       if (!ext) {
         return (
           <div className={cls}>
-            <i className='icon-file-text un-icon' />
-            <span className='un-text'>uknown</span>
+            <i className="icon-file-text un-icon" />
+            <span className="un-text">uknown</span>
           </div>
-        )
+        );
       } else {
         if (imgs.indexOf(ext.toLowerCase()) !== -1) {
-          return <img src={url} alt='' />
+          return <img src={url} alt="" />;
         } else if (videos.indexOf(ext.toLowerCase()) !== -1) {
           return (
             <video>
               <source src={url} />
             </video>
-          )
+          );
         } else if (audios.indexOf(ext.toLowerCase()) !== -1) {
-          return <i className='icon-audio' />
+          return <i className="icon-audio" />;
         } else {
           return (
             <div className={cls}>
-              <i className='icon-file-text un-icon' />
-              <span className='un-text'>.{ext}</span>
+              <i className="icon-file-text un-icon" />
+              <span className="un-text">.{ext}</span>
             </div>
-          )
+          );
         }
       }
     }
   },
-  getMediaComponentByUrl (file, customClass) {
-    const url = file.url[currentLang]
+  getMediaComponentByUrl(file, customClass) {
+    const url = file.url[currentLang];
     if (url) {
       const ext = url
-        .split('/')
+        .split("/")
         .pop()
-        .split('.')
-        .pop()
-      const cls = 'unkownFileType ' + customClass
-      const name = file.name
+        .split(".")
+        .pop();
+      const cls = "unkownFileType " + customClass;
+      const name = file.name;
 
       if (!ext) {
         return (
           <div className={cls}>
-            <i className='icon-file-text un-icon' />
-            <span className='un-text'>uknown</span>
+            <i className="icon-file-text un-icon" />
+            <span className="un-text">uknown</span>
           </div>
-        )
+        );
       } else {
         if (imgs.indexOf(ext.toLowerCase()) !== -1) {
-          return <img src={url} alt='' />
+          return <img src={url} alt="" />;
         } else if (videos.indexOf(ext.toLowerCase()) !== -1) {
           return (
             <video controls>
               <source src={url} />
             </video>
-          )
+          );
         } else if (audios.indexOf(ext.toLowerCase()) !== -1) {
           return (
             <audio controls>
               <source src={url} />
             </audio>
-          )
+          );
         } else {
           return (
             <div className={cls}>
-              <i className='icon-file-text un-icon' />
-              <span className='un-text'>{name}</span>
+              <i className="icon-file-text un-icon" />
+              <span className="un-text">{name}</span>
             </div>
-          )
+          );
         }
       }
     }
   },
-  getMediaThumbnailByUrl (url, customClass) {
+  getMediaThumbnailByUrl(url, customClass) {
     if (url) {
       const ext = url
-        .split('/')
+        .split("/")
         .pop()
-        .split('.')
-        .pop()
+        .split(".")
+        .pop();
 
-      const cls = 'unkownFileType ' + customClass
+      const cls = "unkownFileType " + customClass;
 
       if (!ext) {
         return (
           <div className={cls}>
-            <i className='icon-file-text un-icon' />
-            <span className='un-text'>uknown</span>
+            <i className="icon-file-text un-icon" />
+            <span className="un-text">uknown</span>
           </div>
-        )
+        );
       } else {
         if (imgs.indexOf(ext.toLowerCase()) !== -1) {
-          return <img src={url} alt='' />
+          return <img src={url} alt="" />;
         } else if (videos.indexOf(ext.toLowerCase()) !== -1) {
           return (
-            <div className='thumbnail-video'>
+            <div className="thumbnail-video">
               <video>
                 <source src={url} />
               </video>
-              <div className='thumbnail-cover'>
-                <i className='icon-video' />
+              <div className="thumbnail-cover">
+                <i className="icon-video" />
               </div>
             </div>
-          )
+          );
         } else if (audios.indexOf(ext.toLowerCase()) !== -1) {
-          return <i className='icon-audio' />
+          return <i className="icon-audio" />;
         } else {
           return (
             <div className={cls}>
-              <i className='icon-file-text un-icon' />
-              <span className='un-text'>.{ext}</span>
+              <i className="icon-file-text un-icon" />
+              <span className="un-text">.{ext}</span>
             </div>
-          )
+          );
         }
       }
     }
   },
-  getMediaLocalFile (file, customClass) {
+  getMediaLocalFile(file, customClass) {
     if (file) {
-      const result = window.URL.createObjectURL(file)
-      const type = file.type
-      const cls = 'unkownFileType ' + customClass
+      const result = window.URL.createObjectURL(file);
+      const type = file.type;
+      const cls = "unkownFileType " + customClass;
 
-      if (type.includes('image')) return <img src={result} alt='' />
-      if (type.includes('video')) {
+      if (type.includes("image")) return <img src={result} alt="" />;
+      if (type.includes("video")) {
         return (
           <video>
             <source src={result} />
           </video>
-        )
+        );
       }
-      if (type.includes('audio')) return <i className='icon-audio' />
+      if (type.includes("audio")) return <i className="icon-audio" />;
 
       return (
         <div className={cls}>
-          <i className='icon-file-text un-icon' />
-          <span className='un-text'>{file.name}</span>
+          <i className="icon-file-text un-icon" />
+          <span className="un-text">{file.name}</span>
         </div>
-      )
+      );
     }
   },
-  getMediaLocalFilePreview (file, customClass) {
+  getMediaLocalFilePreview(file, customClass) {
     if (file) {
-      const result = window.URL.createObjectURL(file)
-      const type = file.type
-      const cls = 'unkownFileType ' + customClass
+      const result = window.URL.createObjectURL(file);
+      const type = file.type;
+      const cls = "unkownFileType " + customClass;
 
-      if (type.includes('image')) return <img src={result} alt='' />
-      if (type.includes('video')) {
+      if (type.includes("image")) return <img src={result} alt="" />;
+      if (type.includes("video")) {
         return (
           <video controls>
             <source src={result} />
           </video>
-        )
+        );
       }
-      if (type.includes('audio')) {
+      if (type.includes("audio")) {
         return (
           <audio controls>
             <source src={result} />
           </audio>
-        )
+        );
       }
 
       return (
         <div className={cls}>
-          <i className='icon-file-text un-icon' />
-          <span className='un-text'>{file.name}</span>
+          <i className="icon-file-text un-icon" />
+          <span className="un-text">{file.name}</span>
         </div>
-      )
+      );
     }
   }
-}
+};
